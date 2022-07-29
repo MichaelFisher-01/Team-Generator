1697da1dd1fa8da4b7110abf97d432bec4328ca2
Michael Fisher <fisherdesign3@gmail.com>
2022-07-29 17:22:26 -0600

    Started working out the inquirer prompts to get all the information needed but I am not happy with this format will brainstorm

--- index.js
+++ index.js
@@ -1,25 +1,97 @@
+//Required Installs
 const inquirer = require('inquirer');
+//Required Libraries
 const Employee = require('./lib/employee');
+const Manager = require('./lib/manager');
+const Enginer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+//Global Variabls
+const allManagers = [];
+const allEngineers = [];
+const allInterns = [];
 
+//Aquiring Input
 inquirer
 	.prompt([
 		{
 			type: 'input',
-			message: "What is this employee's name?",
+			message: 'Who manages this team?',
 			name: 'name',
 		},
 		{
 			type: 'input',
-			message: 'What will the employee id be?',
+			message: 'What is the managers employee id?',
 			name: 'id',
 		},
 		{
 			type: 'input',
-			message: "What is this employee's email address?",
+			message: 'What is the managers email address?',
 			name: 'email',
+		},
+		{
+			type: 'input',
+			message: "What is the manager's office number?",
+			name: 'officeNumber',
 		},
 	])
 	.then((response) => {
-		const employee = new Employee(response.name, response.id, response.email);
-		console.log(employee);
+		const manager = new Manager(
+			response.name,
+			response.id,
+			response.email,
+			response.officeNumber
+		);
+		allManagers.push(manager);
+		console.log(allManagers);
 	});
+
+while (addEmployees) {
+	inquirer
+		.prompt([
+			{
+				type: 'confirm',
+				message: 'Would you like to add more employees?',
+				name: 'continue',
+			},
+		])
+		.then((response) => {
+			if (response.continue) {
+				inquirer
+					.prompt([
+						{
+							type: 'list',
+							message: 'What role does this employee have?',
+							choices: ['Engineer', 'Intern'],
+							name: 'role',
+						},
+						{
+							type: 'input',
+							message: "What is this employee's name?",
+							name: 'name',
+						},
+						{
+							type: 'input',
+							message: 'What is thier employee id?',
+							name: 'id',
+						},
+						{
+							type: 'input',
+							message: "What is this employee's email address?",
+							name: 'email',
+						},
+					])
+					.then((response) => {
+						if (response.role === 'Engineer') {
+							inquirer.prompt([
+								{
+									type: 'input',
+									message: 'What is the Engineers github?',
+									name: 'github',
+								},
+							]);
+						} else {
+						}
+					});
+			}
+		});
+}
