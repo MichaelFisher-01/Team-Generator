dae6adf0fe464cf21a720f4d3392eea0d2d7973e
Michael Fisher <fisherdesign3@gmail.com>
2022-08-02 11:25:13 -0600

    Added the writeHTML and writeCSS files. Just have to import them to index.js then run tests

--- src/writeHTML.js
+++ src/writeHTML.js
@@ -1,1 +1,65 @@
-const htmlLayout = () => {};
+const allCards = ``;
+
+const createCard = (allEmployees) => {
+	allEmployees.forEach((employee) => {
+		switch (employee.getRole()) {
+			case 'Manager':
+				icon = '<i class="fa-solid fa-mug-hot"></i>';
+				specificInfo = `<div class="infoBox">OfficeNumber: ${employee.getOfficeNumber()}</div>`;
+				break;
+			case 'Engineer':
+				icon = `<i class="fa-solid fa-glasses"></i>`;
+				specificInfo = `<div class="infoBox">Github: ${employee.getGithub()}$</div>`;
+				break;
+			case 'Intern':
+				icon = `<i class="fa-solid fa-graduation-cap">`;
+				specificInfo = `<div class="infoBox">School: ${employee.getSchool()}</div>`;
+				break;
+			default:
+				console.log('The switch has failed');
+		}
+
+		newCard = `<div class="card">
+        <div class="cardTitle">
+                <div class="name">${employee.getName()}</div>
+                <div class="title">${icon} ${employee.getRole()}</div>
+            </div>
+            <div class="cardBody">
+                <div class="infoBox">ID: ${employee.getId()}</div>
+                <div class="infoBox">
+                    Email:<a href="mailto:jard@gakemail.com"> ${employee.getEmail()}</a>
+                </div>
+                ${specificInfo}
+            </div>
+        </div>`;
+	});
+
+	newCard += allCards;
+};
+
+const html = `<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" href="./style.css" />
+		<link
+			rel="stylesheet"
+			href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"
+		/>
+		<link rel="preconnect" href="https://fonts.googleapis.com" />
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+		<link
+			href="https://fonts.googleapis.com/css2?family=Raleway:wght@600&display=swap"
+			rel="stylesheet"
+		/>
+		<title>My Team</title>
+	</head>
+	<body>
+		<div class="banner">My Team</div>
+		<div class="row">
+			<div class="container">${allCards}</div>
+		</div>
+	</body>
+</html>`;
